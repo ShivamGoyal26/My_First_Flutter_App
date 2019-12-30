a29f32d64cde71afa8bca02f8ba70185b4ed2e42
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2019-12-30 19:33:32 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  
+
+  void answerQuestion() {
+    questionIndex =questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("My First App"),),
-      body: Text("This is my default text!"),
-    ),); 
+    var questions = [
+      "You have choosen the answer 1",
+      "You have choosen the answer 2",
+      "You have choosen the answer 3"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(
+                questions[questionIndex],
+              ),
+              RaisedButton(
+                child: Text("Answer 1"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("Answer 2"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("Answer 3"),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
+    );
   }
 }
