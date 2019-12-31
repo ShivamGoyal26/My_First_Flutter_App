698ede0c93279d3a04fb662595ae3c63f479c118
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2019-12-31 14:00:25 +0530

    5th commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,18 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   var questions = [
-    "what's your favorite color",
-    "what's your favorite city",
-    "what's your favorite car"
+    {
+      'questionText': "what's your favorite color",
+      'answers': ['black', 'red', 'blue'],
+    },
+    {
+      'questionText': "what's your favorite animal",
+      'answers': ['wolf', 'tiger', 'lion'],
+    },
+    {
+      'questionText': "what's your favorite car",
+      'answers': ['lamborgini', 'rolls royce', 'audi'],
+    },
   ];
 
   void _answerQuestion() {
@@ -47,11 +56,37 @@ class _MyAppState extends State<MyApp> {
           body: Column(
             children: <Widget>[
               Question(
-                questions[_questionIndex],
+                questions[_questionIndex]['questionText'],
               ),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
+              // here we map a list of answers into a mapping of widgets with the help of map() function because here map() function return the widget or any thing
+              // here map function will excute for each element in the questions[_questionIndex]['answers'] list  and here the current element recieved in the map function which will be stored in the answer variable you can use any name instead of answer
+              // Here keep in the mind that map() function will excute for every element in the list
+              // here map gives us the Iterable not a list but here we want a list that's why we convert the map in to the lists at last by using tolist() function
+              // now the value which map gives us is a list due to the tolist() function
+              // here column takes the list of widgets but here we have the list of list means nested list
+              // here we use ... operator here this operator takes a list and they put all the values in the list out of it  and add them to the sourrouding list which is chlidren in this case as a individual value
+              // so here technically we dont add the list to list mean children is a list right and we have also a list given below so thats why ... makes the list into the numbers
+                // in short here it will return the widget Answer right but as we discussed that map() function don't return the widgets but it returns iterations right but we want the list of the widgets so we used tolist() function to convert the iterations in to the list of widgets means at last we have the list of widgets now at the top we have the children which is supposed to get the widgets or the values not the list of widgets so that's why we used ... operation to get every widgets from the list of widgets return by map() function and add each widget in the children list now its alright 
+
+              ...(questions[_questionIndex]['answers'] as List<String>)
+                  .map((answer) {
+                return Answer(_answerQuestion, answer);
+              }).toList()
+             
+              // see map() will return this kind of struture to you given below 
+              // [ 
+              //   if _questionIndex = 0
+              //   Answer(_answerQuestion, "black");
+              //   Answer(_answerQuestion, "red");
+              //   Answer(_answerQuestion, "blue");
+              // ]
+
+              // now ... this operator removes the list like 
+              //   Answer(_answerQuestion, "black");
+              //   Answer(_answerQuestion, "red");
+              //   Answer(_answerQuestion, "blue");
+
+
             ],
           )),
     );
